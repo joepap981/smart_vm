8fbc38efded5f1a88dbf02514b5b16413a1b9a46
joepap981 <joepap981@gmail.com>
2019-03-06 14:02:09 +0900

    Revert "[ui] Refactor topnav css"
    This reverts commit 5c468619081b151083a0688354b27fc0649e1198.

--- src/views/index.vue
+++ src/views/index.vue
@@ -19,13 +19,11 @@
             <!--Top Navigation Bar-->
             <div id="topbar">
                 <div class="d-flex justify-content-end mr-4">
-                    <div class="nav-circle-item" id="notification-bell">
-                        <span id="notification-badge" class="badge badge-danger"> 1 </span>
+                    <div id="notification-bell">
+                        <span id="notification-badge" class="badge badge-light"> 1 </span>
                         <img class="nav-img" src="../assets/bell.png" width="30" height="30" style="margin-top: 4px"/>          
                     </div>
-                    <div class="nav-circle-item" id="profile-img">
-                        <img class="nav-img" src="../assets/profile_placeholder.png" width="40" height="40"  />
-                    </div>
+                    <img class="nav-img" src="../assets/profile_placeholder.png" width="40" height="40"  />
                 </div>
             </div>
 
@@ -69,8 +67,6 @@ export default {
         background-color: #EEEEF5;
     }
 
-    /* Side Navbar */
-
     #nav-header {
         height: 60px;
         margin: 0 0 0 0;
@@ -85,6 +81,10 @@ export default {
         background-color: #242939;
         margin: 25px 25px 25px 25px;
         border-radius: 5px 5px 5px 5px;
+    }
+
+    #content {
+        width: 100%;
     }
 
     .nav-group {
@@ -103,26 +103,16 @@ export default {
         color: #D8D8D8;
     }
 
-    #page {
-        padding: 40px 40px 40px 40px;
-    }
-
-    #content {
-        width: 100%;
-    }
-
-    /* Top Navbar */
-
-     #topbar {
+    #topbar {
         margin: 25px 25px 0 0;
         min-height: 60px;
         max-height: 60px;
         min-width: 100%;
         vertical-align: middle;
     }
-    
-    .nav-circle-item {
-        margin: 0 5px 0 5px;
+
+    #page {
+        padding: 40px 40px 40px 40px;
     }
 
     .nav-img {
