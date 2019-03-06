d7a53800f3577fa88b017593fd58559547dc5b54
joepap981 <joepap981@gmail.com>
2019-03-06 14:28:14 +0900

    [component] Work on loading daum map script

--- src/views/Overview/index.vue
+++ src/views/Overview/index.vue
@@ -4,45 +4,25 @@
             <h5> Overview </h5>
         </div>
         <hr />
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-title text-left"> 자판기 </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body ">
-                        <p class="card-title text-left"> 장애현황 </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-title text-left"> 재고현황 </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-title text-left"> 통계 </p>
-                    </div>
-                </div>
-            </div>
+        <div class="card" >
+            <KakaoMap />
         </div>
     </div>
 </template>
 
 <script>
+import KakaoMap from '../../components/KakaoMap.vue'
+
 export default {
-    name: 'Overview'
+    name: 'Overview',
+    components: {
+        KakaoMap
+    }
 }
 </script>
 
 <style scoped>
-
+    .card {
+        border-color: white;
+    }
 </style>
