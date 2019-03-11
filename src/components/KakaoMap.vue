42f2d18093a400b21b85e1215b8dae34af712006
joepap981 <joepap981@gmail.com>
2019-03-11 16:52:04 +0900

    Implement own KakaoMap component
    +add marker set

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -1,45 +1,51 @@
 <template>
-    <div id="map" style="width:100%;height:350px;"></div>
+    <div id="map" style="width:500px;height:400px;"></div>
 </template>
 
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=a2ebbeb486802e2210128b28e3403190"></script>
 <script>
 
 import loadScriptOnce from 'load-script-once';
 
 export default {
     name: 'KakaoMap',
+    data: () => ({
+        map: null,
+        latlng: null,
+    }),
     mounted () {
-        console.log("KakaoMap")
-        var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-        mapOption = { 
-            center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
-        }
-        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-        var map = new daum.maps.Map(mapContainer, mapOption); 
+        loadScriptOnce('//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a2ebbeb486802e2210128b28e3403190', (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-        //var load = require('load-script-once')
- 
-        // load('http://dapi.kakao.com/v2/maps/sdk.js?appkey=a2ebbeb486802e2210128b28e3403190', function (err, script) {
-        //     if (err) {
-        //         // print useful message
-        //         console.log(err)
-        //     }
-        //     else {
-        //         var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-        //         mapOption = { 
-        //             center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        //             level: 3 // 지도의 확대 레벨
-        //         }
+            //once map lib is loaded setup map
+            daum.maps.load(() => {
+                var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+                var options = { //지도를 생성할 때 필요한 기본 옵션
+                    center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+                    level: 7 //지도의 레벨(확대, 축소 정도)
+                };
 
-        //         // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-        //         var map = new daum.maps.Map(mapContainer, mapOption); 
-        //         console.log('loaded script')
-        //     }
-        // })
+                this.map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
 
 
+                //return coordinates on click
+                daum.maps.event.addListener(this.map, 'click', function(mouseEvent) {
+                    var latlng = mouseEvent.latLng;
+                    console.log(latlng);
+                });
+
+                //set up markers
+                var markerPosition  = new daum.maps.LatLng(33.450701, 126.570667); 
+
+                var marker = new daum.maps.Marker({
+                    position: markerPosition
+                });
+
+                marker.setMap(this.map);
+            })
+      });
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,24 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createRouter } from './router'
 
-// var load = require('load-script')
-
-// load('http://dapi.kakao.com/v2/maps/sdk.js?appkey=a2ebbeb486802e2210128b28e3403190', function (err, script) {
-//     if (err) {
-//         // print useful message
-//         console.log(err)
-//     }
-//     else {
-//         console.log('loaded script')
-//     }
-// })
-
-// import loadScriptOnce from 'load-script-once';
-
-// loadScriptOnce('http://dapi.kakao.com/v2/maps/sdk.js?appkey=a2ebbeb486802e2210128b28e3403190')
-// .then(() => console.log('loaded'))
-// .catch(err => console.error('failed to load!', err))
-
 Vue.config.productionTip = false
 
 
