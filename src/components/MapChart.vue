91f83894cc320af35b83280db216b48db1607a9c
joepap981 <joepap981@gmail.com>
2019-03-20 22:19:17 +0900

    Finish mapchart 3 depth movement

--- src/components/MapChart.vue
+++ src/components/MapChart.vue
@@ -46,33 +46,19 @@ export default {
 
         //when zoom out button is pressed, 
         upButton.events.on("hit", function() {
+            //if depth 2, zoom out to the previous
             if (depth > 1) {
-                if (curObject.dataItem.dataContext.zoomLevel != null)
-                    map.zoomToMapObject(curObject, curObject.dataItem.dataContext.zoomLevel)
-                else
-                    map.zoomToMapObject(curObject)
-
-                depth--;
-                map.series.removeIndex(depth);
-                
+                zoomOut(curObject);
             }
-            //zoom out to home
+            //if depth 1 zoom out to home
             else if (depth == 1) {
                 map.series.removeIndex(depth);
                 depth--;
                 curObject = null;
                 map.goHome();
-            }
-            //zoom out to municipality
-            else if (depth > 0) {
-                map.series.removeIndex(depth);
-                depth--;
             } else {
-
+                //do nothing if already home
             }
-
-            console.log(depth)
-
         });
 
         // set series for Seoul municipalities map
@@ -81,7 +67,7 @@ export default {
         map.projection = new am4maps.projections.Miller();
 
         var provinceSeries = map.series.push(new am4maps.MapPolygonSeries());
-        console.log(map.series.length)
+  
         provinceSeries.useGeodata = true;
         var provincePolygonTemplate = provinceSeries.mapPolygons.template;
         provincePolygonTemplate.tooltipText = "{name}";
@@ -92,35 +78,23 @@ export default {
         //zoom in/out to municipality level when clicked
         provinceSeries.mapPolygons.template.events.on("hit", function(ev) {
             curObject = ev.target;
-
-            try {
-                //move to another municipality from submunicipality
-                if (depth > 1) {
-                    depth--;
-                    map.series.removeIndex(depth);
-                }
-                //move from province level to municipality level
-                else if (depth < 1) {
-                    depth++;
-                }
-                //move to another municipality
-                else {
-                    map.series.removeIndex(depth);
-                }
-            } catch (err) {
-                console.log(err)
-                console.log(depth)
-                console.log(curObject)
+            //province(0) -> municipality(1)
+            if (depth == 0) {
+                zoomIn(ev.target);
             }
-
-            console.log(depth);
-           
-            var selectedObjData = ev.target.dataItem.dataContext;
-            if (selectedObjData.zoomLevel != null) {
-                map.zoomToMapObject(ev.target, selectedObjData.zoomLevel)
-            } else {
-                map.zoomToMapObject(ev.target)
+            //municipality(1) -> municipality(1)
+            else if(depth == 1) {
+                zoomMove(ev.target);
+                curObject = ev.target;  
             }
+            //submunicipality(2) -> municipality(1)
+            else {
+                zoomOut(ev.target);
+                zoomMove(ev.target);
+                curObject = ev.target;  
+            }
+            
+
             // set series for municipality for selected provice
             var municipalitySeries = map.series.push(new am4maps.MapPolygonSeries());
             //set template for series geodata
@@ -140,24 +114,18 @@ export default {
 
             //zoom in/out to submunicipality level when clicked
             municipalitySeries.mapPolygons.template.events.on("hit", function(ev) {
-                //if move to another municipality in depth
-                if (depth > 1) {
-                    map.series.removeIndex(depth-1);
-                } else if (depth < 1) {
-                    depth++;
-                } else {
-                    map.series.removeIndex(depth);
+                //municipality(1) -> submunicipality(2)
+                if (depth == 1){
+                    zoomIn(ev.target);
                 }
+                //submunicipality(2) -> another.submunicipality(2)
+                else if (depth == 2) {
+                    zoomMove(ev.target)
+                }
+               
 
-                depth++;
-                console.log(depth);
+                // zoomIn(ev.target);
                 
-                let selectedObjData = ev.target.dataItem.dataContext;
-                if (selectedObjData.zoomLevel != null) {
-                    map.zoomToMapObject(ev.target, selectedObjData.zoomLevel)
-                } else {
-                    map.zoomToMapObject(ev.target)
-                }
                 // set series for municipality for selected provice
                 let submunicipalitySeries = map.series.push(new am4maps.MapPolygonSeries());
                 //set template for series geodata
@@ -176,45 +144,38 @@ export default {
             });
         });
 
+        var zoomMove = function (target) {
+            map.series.removeIndex(depth);
+            let selectedObjData = target.dataItem.dataContext;
+                if (selectedObjData.zoomLevel != null) {
+                    map.zoomToMapObject(target, selectedObjData.zoomLevel)
+                } else {
+                    map.zoomToMapObject(target)
+            }
+        }
+        
+        var zoomIn = function (target) {
+            depth++;
+            let selectedObjData = target.dataItem.dataContext;
+                if (selectedObjData.zoomLevel != null) {
+                    map.zoomToMapObject(target, selectedObjData.zoomLevel)
+                } else {
+                    map.zoomToMapObject(target)
+            }
 
+        };
 
-
-        // // set series for South Korea
-        // var seoulSeries = map.series.push(new am4maps.MapPolygonSeries());
-        // seoulSeries.useGeodata = true;
-    
-        // var seoulPolygonTemplate = seoulSeries.mapPolygons.template;
-        // seoulPolygonTemplate.tooltipText = "{name}";
-        // seoulPolygonTemplate.fill = am4core.color("#113f67");
-
-        // var hs = seoulPolygonTemplate.states.create("hover");
-        // hs.properties.fill = am4core.color("#226597");
-
-        // //zoom into municipality to show submuniciple
-        // seoulSeries.mapPolygons.template.events.on("hit", function(ev) {
-        //     //remove loaded submunicipality series
-        //     if (map.series._values.length > 1)
-        //         map.series.removeIndex(1);
-
-        //     map.zoomToMapObject(ev.target);
-
-        //     // set series for Seoul submunicipalities map
-        //     var seoulSubSeries = map.series.push(new am4maps.MapPolygonSeries());
-        //     seoulSubSeries.geodata = {"type":"FeatureCollection", "features": [
-        //     ]}
-
-        //     //filter submunicipality that belongs to selected municipality
-        //     seoulSubSeries.geodata.features = am4geodata_seoulSubmunicipalities.features.filter(features => {
-        //         return features.properties.municipality == ev.target.dataItem.dataContext.id;
-        //     });
-                
-        //     var seoulSubPolygonTemplate = seoulSubSeries.mapPolygons.template;
-        //     seoulSubPolygonTemplate.tooltipText = "{name}";
-        //     seoulSubPolygonTemplate.fill = am4core.color("#87c0cd");
-
-        //     var hs2 = seoulSubPolygonTemplate.states.create("hover");
-        //     hs2.properties.fill = am4core.color("#f3f9fb");
-        // });
+        var zoomOut = function (target) {
+            map.series.removeIndex(depth);
+            depth--;
+           
+            let selectedObjData = target.dataItem.dataContext;
+                if (selectedObjData.zoomLevel != null) {
+                    map.zoomToMapObject(target, selectedObjData.zoomLevel)
+                } else {
+                    map.zoomToMapObject(target)
+            }
+        }
 
     }, beforeDestroy() {
         if (this.chart)
